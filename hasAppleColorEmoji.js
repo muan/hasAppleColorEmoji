58fa228c2d7705be69c3bec1a2882b9583ed7d88
Mu-An Chiou <me@muanchiou.com>
2015-06-19 17:18:00 -0400

    Single quotes

--- hasAppleColorEmoji.js
+++ hasAppleColorEmoji.js
@@ -2,8 +2,8 @@ function hasAppleColorEmoji () {
   var widths = []
   tags = [document.createElement('span'), document.createElement('span')]
   tags.forEach(function(tag, i) {
-    tag.innerText = "️️️☺︎"
-    tag.style.fontFamily = i === 1 ? "thisisnotafont" : "AppleColorEmoji"
+    tag.innerText = '☺'
+    tag.style.fontFamily = i === 1 ? 'thisisnotafont' : 'AppleColorEmoji'
     document.body.appendChild(tag)
     widths.push(tag.offsetWidth)
     tag.remove()
